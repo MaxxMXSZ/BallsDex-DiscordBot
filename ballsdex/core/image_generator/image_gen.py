d61a0f0cabe2e795431149cfad17fd341293896b
Mxlodix <153226500+MaxxMXSZ@users.noreply.github.com>
2025-02-06 22:32:38 +0100

    Update image_gen.py

--- ballsdex/core/image_generator/image_gen.py
+++ ballsdex/core/image_generator/image_gen.py
@@ -26,6 +26,14 @@ stats_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 130)
 credits_font = ImageFont.truetype(str(SOURCES_PATH / "arial.ttf"), 40)
 
 
+def get_credit_color(image: Image.Image, region: tuple) -> tuple:
+    cropped = image.crop(region)
+    avg_color = tuple(sum(cropped.getdata(), (0, 0, 0))[:3])
+    avg_brightness = sum(avg_color) / (3 * len(cropped.getdata()))
+
+    return (255, 255, 255) if avg_brightness < 100 else (0, 0, 0)
+
+
 def draw_card(ball_instance: "BallInstance", media_path: str = "./admin_panel/media/"):
     ball = ball_instance.countryball
     ball_health = (237, 115, 101, 255)
@@ -86,6 +94,7 @@ def draw_card(ball_instance: "BallInstance", media_path: str = "./admin_panel/me
         stroke_fill=(0, 0, 0, 255),
         anchor="ra",
     )
+    credits_color = get_credit_color(image, (0, int(image.height * 0.8), image.width, image.height))
     draw.text(
         (30, 1870),
         # Modifying the line below is breaking the licence as you are removing credits
